47d9ffca486076b7673b132e9d0a6de72e1441d9
Mark G <mark.goodbody@gmail.com>
2026-09-17 19:00:24 +0100

    ATRS membership: test utility-link exclusion

--- competition/atrs_answerability/test_membership.py
+++ competition/atrs_answerability/test_membership.py
@@ -12,33 +12,35 @@ SPEC.loader.exec_module(mod)
 
 PAGE = '''
 <html><body>
-<h3>141 records</h3>
+<h3>152 records</h3>
 <a href="/algorithmic-transparency-records/tool-a">Tool A</a>
 <a href='/algorithmic-transparency-records/tool-b'>Tool B</a>
 <a href="/algorithmic-transparency-records/tool-a">Tool A duplicate link</a>
-<a href="?page=2" rel="next">Next page: 2 of 3</a>
+<a href="/algorithmic-transparency-records/email-signup">Get emails</a>
+<a href="?page=2" rel="next">Next page: 2 of 4</a>
 </body></html>
 '''
 LAST = '''
-<html><body><h2>141 records</h2>
+<html><body><h2>152 records</h2>
 <a href="/algorithmic-transparency-records/tool-c">Tool C</a>
+<a href="/algorithmic-transparency-records/email-signup">Subscribe</a>
 </body></html>
 '''
 
 class MembershipTests(unittest.TestCase):
-    def test_parse_finder_page_deduplicates_record_links(self):
+    def test_parse_finder_page_deduplicates_and_excludes_utility_links(self):
         urls, count, next_page = mod.parse_finder_page(PAGE)
         self.assertEqual(urls, [
             "https://www.gov.uk/algorithmic-transparency-records/tool-a",
             "https://www.gov.uk/algorithmic-transparency-records/tool-b",
         ])
-        self.assertEqual(count, 141)
+        self.assertEqual(count, 152)
         self.assertEqual(next_page, 2)
 
     def test_last_page_has_no_next(self):
         urls, count, next_page = mod.parse_finder_page(LAST)
-        self.assertEqual(len(urls), 1)
-        self.assertEqual(count, 141)
+        self.assertEqual(urls, ["https://www.gov.uk/algorithmic-transparency-records/tool-c"])
+        self.assertEqual(count, 152)
         self.assertIsNone(next_page)
 
 if __name__ == "__main__":
